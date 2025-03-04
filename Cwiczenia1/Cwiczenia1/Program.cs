4de8da88fd660150f6185a6ad50e8d79338f1b21
s30580 <michal.soltysiak03@gmial.com>
2025-03-04 15:09:51 +0100

    END

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -3,3 +3,17 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1 pozdrawiam");
 Console.WriteLine("Modyfikacja 2 pozdrawiam");
 Console.WriteLine("Modyfikacja 3 pozdrawiam");
+
+
+static double Function1(int[] arr)
+{
+    double ret = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        ret += arr[i];
+    }
+    ret /= arr.Length;
+    return ret;
+}
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+Console.WriteLine(Function1(arr));
