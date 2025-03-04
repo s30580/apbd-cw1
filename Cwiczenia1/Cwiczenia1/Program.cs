9719ef16eacfa9716219dc5b6aa3879c395178b6
s30580 <michal.soltysiak03@gmial.com>
2025-03-04 14:52:13 +0100

    Modyfikacja 2

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1 pozdrawiam");
+Console.WriteLine("Modyfikacja 2 pozdrawiam");
